a9331026f244606af9e3cd54d4b294565722febc
Thibault Doucet <thibault.doucet44@gmail.com>
2021-02-08 09:34:04 +0100

    :sparkles: Add Game repo and entity

--- src/main/java/fr/epsi/apiweb/resources/dto/Game.java
+++ src/main/java/fr/epsi/apiweb/resources/dto/Game.java
@@ -1,2 +1,22 @@
-package fr.epsi.apiweb.resources.dto;public class Game {
+package fr.epsi.apiweb.resources.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Game {
+
+    @Id
+    private UUID game_id;
+    private String name;
+    private boolean is_online;
+    private String description;
+
 }
--- src/main/java/fr/epsi/apiweb/resources/repositories/GamesRepository.java
+++ src/main/java/fr/epsi/apiweb/resources/repositories/GamesRepository.java
@@ -1,2 +1,13 @@
-package fr.epsi.apiweb.resources.repositories;public class GamesRepository {
+package fr.epsi.apiweb.resources.repositories;
+
+import fr.epsi.apiweb.resources.dto.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface GamesRepository extends JpaRepository<Game, UUID> {
+    Optional<Game> findByGameId(UUID id);
 }
