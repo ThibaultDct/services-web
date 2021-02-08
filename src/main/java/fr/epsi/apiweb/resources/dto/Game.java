3b558a97cb5971f1ec34d28b882e62d9438edb99
Thibault Doucet <thibault.doucet44@gmail.com>
2021-02-08 10:10:23 +0100

    :sparkles: Games minor changes

--- src/main/java/fr/epsi/apiweb/resources/dto/Game.java
+++ src/main/java/fr/epsi/apiweb/resources/dto/Game.java
@@ -2,21 +2,52 @@ package fr.epsi.apiweb.resources.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
 public class Game {
 
     @Id
+    @Generated(GenerationTime.ALWAYS)
     private UUID game_id;
     private String name;
     private boolean is_online;
     private String description;
 
+    public UUID getGame_id() {
+        return game_id;
+    }
+
+    public void setGame_id(UUID game_id) {
+        this.game_id = game_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isIs_online() {
+        return is_online;
+    }
+
+    public void setIs_online(boolean is_online) {
+        this.is_online = is_online;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/fr/epsi/apiweb/service/GamesService.java
+++ src/main/java/fr/epsi/apiweb/service/GamesService.java
@@ -1,2 +1,51 @@
-package fr.epsi.apiweb.service;public class GamesService {
+package fr.epsi.apiweb.service;
+
+import fr.epsi.apiweb.resources.dto.Game;
+import fr.epsi.apiweb.resources.repositories.GamesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class GamesService {
+
+    /* LOGGER */
+    Logger LOGGER = LoggerFactory.getLogger(GamesService.class);
+
+    @Autowired
+    private GamesRepository gamesRepository;
+
+    public void create(String name, boolean is_online, String description){
+        Game gameToCreate = new Game();
+
+        gameToCreate.setName(name);
+        gameToCreate.setIs_online(is_online);
+        gameToCreate.setDescription(description);
+
+        try {
+            gamesRepository.save(gameToCreate);
+            LOGGER.info("Game {} created", gameToCreate.getName());
+        } catch (Exception e) {
+            LOGGER.error("An error occured while creating game {} : {}", gameToCreate.getName(), e.getMessage());
+        }
+
+    }
+
+    public Game get(UUID id){
+        Game result = null;
+
+        Optional<Game> query = gamesRepository.findByGameId(id);
+        if (query.isPresent()){
+            result = query.get();
+        } else {
+            LOGGER.error("Unable to get game with UUID {}", id);
+        }
+
+        return result;
+    }
+
 }
