da14dbd672ca8ee0888848ff9778402afaeef20e
AlexE1998 <aernault44@gmail.com>
2021-02-08 10:58:48 +0100

    Ajout class Avis et Repo AvisRepository

---
+++ src/main/java/fr/epsi/apiweb/resources/repositories/AvisRepository.java
@@ -0,0 +1,13 @@
+package fr.epsi.apiweb.resources.repositories;
+
+import fr.epsi.apiweb.resources.dto.Avis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AvisRepository extends JpaRepository<Avis, UUID> {
+    Optional<Avis> findByAvisId(UUID id);
+}
